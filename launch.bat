9addbd2d5af2bc2d45a31a1e6295e721962e4e9c
Yannick Formaggio <yannick.formaggio@gmail.com>
2016-11-04 14:33:52 -0700

    removed winappdbg ontroller

--- launch.bat
+++ launch.bat
@@ -1,2 +1,2 @@
 @echo off
-python nmfuzz\fuzz.py -p C:\NetworkMiner_2-0\NetworkMiner.exe -c C:\Users\yformaggio\workspace\projects\pcap\pcap\out
+python nmfuzz\fuzz.py -p C:\NetworkMiner_2-0\NetworkMiner.exe -c C:\Users\yannick\workspace\fuzzing\pcap\triage.valid
